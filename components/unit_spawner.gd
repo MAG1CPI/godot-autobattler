18572a854c7c1278ff5b626d8ecc3e7f762301e4
pi <2947928186@qq.com>
2026-02-14 10:37:44 +0800

    Added unit spawner

---
+++ components/unit_spawner.gd
@@ -0,0 +1,33 @@
+extends Node
+class_name UnitSpawner
+
+signal unit_spawned(unit: Unit)
+
+const UNIT = preload("uid://wkov578c00np")
+
+@export var bench: PlayArea
+@export var game_area: PlayArea
+
+
+#func _ready() -> void:
+  #const ROBIN = preload("uid://cejvu4vywgku7")
+  #var tween:=create_tween()
+  #for i in 15:
+    #tween.tween_callback(spawn_unit.bind(ROBIN))
+    #tween.tween_interval(0.5)
+
+func _get_first_avilable_area() -> PlayArea:
+  return bench if not bench.unit_grid.is_grid_full() \
+    else game_area if not game_area.unit_grid.is_grid_full() \
+    else null
+
+func spawn_unit(unit_stats: UnitStats) -> void:
+  var area := _get_first_avilable_area()
+  assert(area, "NO AVAILABLE SPACE TO ADD UNIT TO!") # TODO
+  var new_unit := UNIT.instantiate()
+  var tile := area.unit_grid.get_first_empty_tile()
+  area.unit_grid.add_child(new_unit)
+  area.unit_grid.add_unit(tile, new_unit)
+  new_unit.global_position = area.get_global_from_tile(tile) - Arena.HALF_CELL_SIZE
+  new_unit.stats = unit_stats
+  unit_spawned.emit(new_unit)
--- scenes/arena/arena.gd
+++ scenes/arena/arena.gd
@@ -5,3 +5,14 @@ class_name Arena
 const CELL_SIZE := Vector2(32, 32)
 const HALF_CELL_SIZE := CELL_SIZE / 2
 const QUARTER_CELL_SIZE := HALF_CELL_SIZE / 2
+
+
+@onready var visuals: Node2D = $Visuals
+@onready var game_area: PlayArea = $GameArea
+@onready var bench: PlayArea = $Bench
+@onready var unit_mover: UnitMover = $UnitMover
+@onready var unit_spawner: UnitSpawner = $UnitSpawner
+
+
+func _ready() -> void:
+  unit_spawner.unit_spawned.connect(unit_mover.setup_unit)
